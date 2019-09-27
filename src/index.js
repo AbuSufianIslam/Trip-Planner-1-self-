f486493794026594a6cfbce2e9693ba3cbd7d818
AbuSufianIslam <thebooming1992@gmail.com>
2019-09-27 10:56:07 -0400

    add api token

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello from JavaScript');
+//console.log('Hello from JavaScript');
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+	'pk.eyJ1IjoiYWJ1c3VmaWFuaXNsYW0iLCJhIjoiY2swenN4YWlnMHM5ajNsbzV2NHEyM3FncSJ9.wKxlavfgHkP0fPU5tHJThA';
+
+const map = new mapboxgl.Map({
+	container: 'map',
+	center: [ -74.009, 40.705 ], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+	zoom: 12, // starting zoom
+	style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
